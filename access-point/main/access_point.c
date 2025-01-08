9ed6ca9e3fb96cc668ace42d8a28704bc7cbce12
Jared Amaral <jaredamaral1229@gmail.com>
2025-01-07 18:35:29 -0800

    feat: created access point server

---
+++ access-point/main/access_point.c
@@ -0,0 +1,38 @@
+#include "access_point_server.h"
+
+static const char *TAG = "wifi softAP";
+
+void app_main(void)
+{
+  //Initialize NVS
+  esp_err_t ret = nvs_flash_init();
+  if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+    ESP_ERROR_CHECK(nvs_flash_erase());
+    ret = nvs_flash_init();
+  }
+  ESP_ERROR_CHECK(ret);
+
+  ESP_LOGI(TAG, "ESP_WIFI_MODE_AP");
+  wifi_init_softap();
+
+  
+  // Initialize UDP Server
+  ESP_ERROR_CHECK(nvs_flash_init());
+  ESP_ERROR_CHECK(esp_netif_init());
+  //ESP_ERROR_CHECK(esp_event_loop_create_default());
+
+  /* This helper function configures Wi-Fi or Ethernet, as selected in menuconfig.
+   * Read "Establishing Wi-Fi or Ethernet Connection" section in
+   * examples/protocols/README.md for more information about this function.
+   */
+  //ESP_ERROR_CHECK(example_connect());
+
+  #ifdef CONFIG_EXAMPLE_IPV4
+    xTaskCreate(udp_server_task, "udp_server", 4096, (void*)AF_INET, 5, NULL);
+  #endif
+  #ifdef CONFIG_EXAMPLE_IPV6
+    xTaskCreate(udp_server_task, "udp_server", 4096, (void*)AF_INET6, 5, NULL);
+  #endif
+}
+
+
---
+++ access-point/main/access_point_server.h
@@ -0,0 +1,26 @@
+#ifndef ACCESS_POINT_SERVER_H_
+#define ACCESS_POINT_SERVER_H_
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+#include <sys/param.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_mac.h"
+#include "esp_system.h"
+#include "esp_wifi.h"
+#include "esp_event.h"
+#include "esp_log.h"
+#include "nvs_flash.h"
+#include "lwip/err.h"
+#include "lwip/sys.h"
+
+#include "lwip/sockets.h"
+#include <lwip/netdb.h>
+
+void udp_server_task(void *);
+void wifi_event_handler(void *, esp_event_base_t, int32_t, void *);
+void wifi_init_softap(void);
+
+#endif // ACCESS_POINT_SERVER_H_
