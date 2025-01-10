53bea09e448cc910549e6f983adca19e5cf3ed10
Jared Amaral <jaredamaral1229@gmail.com>
2025-01-09 20:03:13 -0800

    fix: update build and CMake files

--- access-point/build/esp-idf/cmake_install.cmake
+++ access-point/build/esp-idf/cmake_install.cmake
@@ -539,6 +539,16 @@ endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for the subdirectory.
+  include("/home/szensei/Projects/wifi_walkie/access-point/build/esp-idf/espressif__cmake_utilities/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("/home/szensei/Projects/wifi_walkie/access-point/build/esp-idf/espressif__button/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
   include("/home/szensei/Projects/wifi_walkie/access-point/build/esp-idf/main/cmake_install.cmake")
 endif()
 
